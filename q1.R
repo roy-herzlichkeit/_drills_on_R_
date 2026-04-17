c59efa11d05d399293c6a2c7e2789d75db9dce6d
Roy@Souharda <b123134@iiit-bh.ac.in>
2026-04-17 21:18:26 +0530

    C1

---
+++ q1.R
@@ -0,0 +1,4 @@
+name <- readline(prompt = "What is your name?\n")
+age <- readline(prompt = "What is your age?\n")
+print(paste("Du heisst", name, "und du bist", age, "jahre alt.", sep = " "))
+print(R.version.string)
