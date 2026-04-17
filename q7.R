ba62e490ff9c12cc2b5547327b1f5386df1c24ef
Roy@Souharda <b123134@iiit-bh.ac.in>
2026-04-17 22:21:43 +0530

    C7

---
+++ q7.R
@@ -0,0 +1,12 @@
+print_factors = function(n) {
+  print(paste("The factors of", n, "are: "))
+  for (i in 1:n) {
+    if ((n %% i) == 0) {
+      print(i)
+    }
+  }
+}
+
+print_factors(12)
+
+print_factors(3301)
